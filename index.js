088156e73945844586c68bd284be3c017893d72b
Maxim Predko <maxpredko@gmail.com>
2022-07-19 11:51:18 +0300

    add response if error

--- index.js
+++ index.js
@@ -6,28 +6,28 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 3010
 let smtp_login = process.env.SMTP_LOGIN || "maxpayne0689@gmail.com"
-let smtp_password = process.env.SMTP_PASS ||  "mstxboxtuxcmppqp"
+let smtp_password = process.env.SMTP_PASS || "mstxboxtuxcmppqp"
 
 app.use(cors())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
 let transporter = nodemailer.createTransport({
-    // host: "smtp.gmail.com",
-    // port: 465,
-    // secure: false, // true for 465, false for other ports
-    service: 'gmail', //smtp.gmail.com
-    secure: false, //true
-    port: 25, //465
-        auth: {
-      user: smtp_login , 
-      pass:smtp_password, 
-    },
-    tls: {
-        rejectUnauthorized: false
-    }
-  });
+  // host: "smtp.gmail.com",
+  // port: 465,
+  // secure: false, // true for 465, false for other ports
+  service: 'gmail', //smtp.gmail.com
+  secure: false, //true
+  port: 25, //465
+  auth: {
+    user: smtp_login,
+    pass: smtp_password,
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
+});
 
 
 app.get('/', (req, res) => {
@@ -35,31 +35,31 @@ app.get('/', (req, res) => {
 })
 
 app.post('/send-message', async (req, res) => {
-   
-    let {sender, email, subject, phone, textMessage } = req.body
 
-    let mailLetter = {
-        from: 'MY PORTFOLIO',
-        to: "maxpayne0689@gmail.com",
-        subject: subject,
-        // text: "response ==> OK. Testing send messages to mail",
-        html: `<b>Message from your portfolio!</b>
+  let { sender, email, subject, phone, textMessage } = req.body
+
+  let mailLetter = {
+    from: 'MY PORTFOLIO',
+    to: "maxpayne0689@gmail.com",
+    subject: subject,
+    // text: "response ==> OK. Testing send messages to mail",
+    html: `<b>Message from your portfolio!</b>
         <div><b>name:</b> ${sender}</div>
         <div><b>phone:</b> ${phone}</div>
         <div><b>subject</b>: ${subject}</div>
         <hr/>
         <div>${textMessage}</div>`
-    } 
-    
+  }
 
-    await transporter.sendMail(mailLetter, function(err, data) {
-        err ? console.log('Error occurs') : console.log('Email send successfully')
-        }
-    )
 
-    res.send('Send success!')
+  await transporter.sendMail(mailLetter, function (err, data) {
+    err ? res.send('Error occured') : res.send('Email send successfully')
+  }
+  )
+
+  // res.send('Send success!')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
